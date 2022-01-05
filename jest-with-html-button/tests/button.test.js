e789df0aec2439a102be66a3a6aa5788a20377a9
BrianStritch <brians12ca18@gmail.com>
2022-01-05 20:50:28 +0000

    add content to button and button tests to test html interactivity

---
+++ jest-with-html-button/tests/button.test.js
@@ -0,0 +1,19 @@
+
+   
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button");
+
+beforeAll(() => {  // does not take any arguements or parameters and this sets what the initial value is before any changes are made
+    document.body.innerHTML = "<p id='par'></p>";
+});
+
+describe("DOM tests", () => {
+    test("Expects content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par")
+            .innerHTML).toEqual("You Clicked");
+    });
+});
--- scripts/calc.js
+++ scripts/calc.js
@@ -3,3 +3,22 @@ function addition(num1, num2) {
 };
 
 module.exports = addition; // add this at the bottom of the page to export the function for testing
+
+function subtraction (num1, num2) {
+    return num1 - num2;
+};
+
+module.exports = subtraction; // add this at the bottom of the page to export the function for testing
+
+function multiply (num1, num2) {
+    return num1 * num2;
+};
+
+module.exports = multiply; // add this at the bottom of the page to export the function for testing
+
+function division (num1, num2) {
+    return num1 / num2;
+};
+
+module.exports = division; // add this at the bottom of the page to export the function for testing
+
